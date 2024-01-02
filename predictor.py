f22e5b6d5e7e237cb7af108fabe9177a03ddf18d
quocanh34 <phamquocanh2002ct@gmail.com>
2024-01-02 15:11:39 +0300

    add try catch block

--- predictor.py
+++ predictor.py
@@ -215,6 +215,11 @@ class Predictor(BasePredictor):
             description="stop_text_encoder_training ratio",
             default=0.8,
         ),
+
+        noise_offset: float = Input(
+            description="noise_offset",
+            default=0.0,
+        ),
         # scale_lr: bool = Input(
         #     description="Scale the learning rate by the number of GPUs, gradient accumulation steps, and batch size.",
         #     default=False,
@@ -413,7 +418,7 @@ class Predictor(BasePredictor):
             # "cache_latents_to_disk": True,
             "xformers": True,
             "bucket_no_upscale": True,
-            # "noise_offset": 0.0,
+            "noise_offset": noise_offset,
             # "adaptive_noise_scale": True,
             "await-explicit-shutdown":True,
             "upload-url":"http://api.tenant-replicate-prdsvcs.svc.cluster.local/_internal/file-upload/"
