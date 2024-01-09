89d70dd7667ade47e0d987a8d2587d79dc6f03eb
quocanh34 <phamquocanh2002ct@gmail.com>
2024-01-09 03:03:49 +0300

    fix wget

--- augment_data.py
+++ augment_data.py
@@ -28,8 +28,8 @@ class FaceCropper():
                 cropped_array = self.cropper.crop(f"{image_folder}/{filename}")
                 try:
                     cropped_image = Image.fromarray(cropped_array)
-                    print(f"Upscaling cropped images {index} ...")
-                    cropped_image = self.upscaler.upscale(cropped_image)
+                    # print(f"Upscaling cropped images {index} ...")
+                    # cropped_image = self.upscaler.upscale(cropped_image)
                     cropped_images_list.append(cropped_image)
                     cropped_image.save(f"{image_folder}/cropped_img_{index}.jpg")
                     cropped_count+=1
